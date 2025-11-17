824964f17604982e3af35e3454a9506911257775
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-11-17 19:14:06 +0200

    fix

--- app/lib/endpoints.ts
+++ app/lib/endpoints.ts
@@ -2,7 +2,7 @@ import type { Route } from "next";
 
 export const ENDPOINTS = {
   simpleStream: "/api/simple-stream",
-  streamPoll: "/api/stream-poll",
-  streamStart: "/api/stream-start",
+  streamPoll: "/api/stream/poll",
+  streamStart: "/api/stream/start",
   root: "/",
 } as const satisfies Record<string, Route>;
